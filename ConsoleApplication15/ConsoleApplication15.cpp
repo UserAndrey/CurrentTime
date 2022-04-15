065a9f5562748b465092126f28f1c3c1b62e065e
User <User@WIN-A4TOTEJDR9Q>
2022-04-15 21:55:04 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication15/ConsoleApplication15.cpp
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+    time_t now = time(0);
+    char* dt = ctime(&now);
+    cout << dt<<"Current time"<< endl;
+    tm* gmtm = gmtime(&now);
+    dt = asctime(gmtm);
+    cout << dt<<"GMT 0" << endl;
+}
